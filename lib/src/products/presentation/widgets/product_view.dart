d86f477453f5de7d913b8dc54ed3f8df7e7d080c
Juho Simojoki <juho.simojoki@outlook.com>
2024-03-13 11:47:09 +0200

    Separated choice of visible categories into separate files
    Helps in making responsive design easier. Now, depending on screen width, the way options are shown can differ.

--- lib/src/products/presentation/widgets/product_view.dart
+++ lib/src/products/presentation/widgets/product_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:state_training/src/products/presentation/state/category_state.dart';
 import 'package:state_training/src/products/presentation/state/product_state.dart';
 import 'package:state_training/src/products/presentation/widgets/product_form/add_product_form.dart';
 import 'package:state_training/src/products/presentation/widgets/product_list/product_list_medium.dart';
 import 'package:state_training/src/products/presentation/widgets/product_list/product_list_small.dart';
+import 'package:state_training/src/products/presentation/widgets/visible_category_options/choose_category_medium.dart';
+import 'package:state_training/src/products/presentation/widgets/visible_category_options/choose_category_small.dart';
 
 class ProductView extends StatefulWidget {
   const ProductView({super.key});
@@ -15,12 +16,16 @@ class ProductView extends StatefulWidget {
 
 class _ProductViewState extends State<ProductView> {
   var chosenCategory = 'Show all';
+  void setCategory(String value) {
+    setState(() {
+      chosenCategory = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var productState = context.watch<ProductState>();
     var products = productState.products;
-    var categoryState = context.watch<CategoryState>();
-    var categoryNames = categoryState.categoryNames;
     var visibleProducts = products.where((product) {
       return chosenCategory == 'Show all'
           ? true
@@ -32,21 +37,7 @@ class _ProductViewState extends State<ProductView> {
         return Column(
           children: [
             const AddProductForm(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Flex(
-                  direction: Axis.horizontal,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: categoryNames
-                      .map((e) => FloatingActionButton(
-                          onPressed: () {
-                            setState(() {
-                              chosenCategory = e;
-                            });
-                          },
-                          child: Text(e)))
-                      .toList()),
-            ),
+            ChooseCategorySmall(setCategory: setCategory),
             ProductListSmall(visibleProducts: visibleProducts)
           ],
         );
@@ -54,21 +45,7 @@ class _ProductViewState extends State<ProductView> {
         return Column(
           children: [
             const AddProductForm(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Flex(
-                  direction: Axis.horizontal,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: categoryNames
-                      .map((e) => FloatingActionButton(
-                          onPressed: () {
-                            setState(() {
-                              chosenCategory = e;
-                            });
-                          },
-                          child: Text(e)))
-                      .toList()),
-            ),
+            ChooseCategoryMedium(setCategory: setCategory),
             ProductListMedium(visibleProducts: visibleProducts)
           ],
         );
---
+++ lib/src/products/presentation/widgets/visible_category_options/choose_category_small.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_training/src/products/presentation/state/category_state.dart';
+
+class ChooseCategorySmall extends StatelessWidget {
+  const ChooseCategorySmall({
+    required this.setCategory,
+    super.key,
+  });
+  final void Function(String) setCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    var categoryNames = context.watch<CategoryState>().categoryNames;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Flex(
+          direction: Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: categoryNames
+              .map((e) => FloatingActionButton(
+                  onPressed: () {
+                    setCategory(e);
+                  },
+                  child: Text(e)))
+              .toList()),
+    );
+  }
+}
