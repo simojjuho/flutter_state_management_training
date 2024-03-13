f4e1115741b8147d20b4cb3dc46306dd1d22ea64
Juho Simojoki <juho.simojoki@outlook.com>
2024-03-13 11:17:27 +0200

    Added more responsiveness into product view
    Added LayoutBuilder into ProductView and separated product listing into its own Widget so that it can be more easily be changed depending on how wide the screen is.

---
+++ lib/src/products/presentation/widgets/product_list/product_list_medium.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:state_training/src/products/domain/core_entities/product.dart';
+import 'package:state_training/src/products/presentation/widgets/product_tile.dart';
+
+class ProductListMedium extends StatelessWidget {
+  const ProductListMedium({
+    required this.visibleProducts,
+    super.key,
+  });
+  final Iterable<Product> visibleProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.count(
+        padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+        shrinkWrap: true,
+        crossAxisCount: 3,
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5.0,
+        childAspectRatio: 2.5,
+        children: visibleProducts
+            .map(
+              (e) => ProductTile(product: e),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
--- lib/src/products/presentation/widgets/product_view.dart
+++ lib/src/products/presentation/widgets/product_view.dart
@@ -3,7 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:state_training/src/products/presentation/state/category_state.dart';
 import 'package:state_training/src/products/presentation/state/product_state.dart';
 import 'package:state_training/src/products/presentation/widgets/product_form/add_product_form.dart';
-import 'package:state_training/src/products/presentation/widgets/product_tile.dart';
+import 'package:state_training/src/products/presentation/widgets/product_list/product_list_medium.dart';
+import 'package:state_training/src/products/presentation/widgets/product_list/product_list_small.dart';
 
 class ProductView extends StatefulWidget {
   const ProductView({super.key});
@@ -26,40 +27,52 @@ class _ProductViewState extends State<ProductView> {
           : product.category.name == chosenCategory;
     });
 
-    return Column(
-      children: [
-        const AddProductForm(),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Flex(
-              direction: Axis.horizontal,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: categoryNames
-                  .map((e) => FloatingActionButton(
-                      onPressed: () {
-                        setState(() {
-                          chosenCategory = e;
-                        });
-                      },
-                      child: Text(e)))
-                  .toList()),
-        ),
-        Expanded(
-          child: GridView.count(
-            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-            shrinkWrap: true,
-            crossAxisCount: 3,
-            mainAxisSpacing: 5.0,
-            crossAxisSpacing: 5.0,
-            childAspectRatio: 2.5,
-            children: visibleProducts
-                .map(
-                  (e) => ProductTile(product: e),
-                )
-                .toList(),
-          ),
-        )
-      ],
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 600) {
+        return Column(
+          children: [
+            const AddProductForm(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: categoryNames
+                      .map((e) => FloatingActionButton(
+                          onPressed: () {
+                            setState(() {
+                              chosenCategory = e;
+                            });
+                          },
+                          child: Text(e)))
+                      .toList()),
+            ),
+            ProductListSmall(visibleProducts: visibleProducts)
+          ],
+        );
+      } else {
+        return Column(
+          children: [
+            const AddProductForm(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: categoryNames
+                      .map((e) => FloatingActionButton(
+                          onPressed: () {
+                            setState(() {
+                              chosenCategory = e;
+                            });
+                          },
+                          child: Text(e)))
+                      .toList()),
+            ),
+            ProductListMedium(visibleProducts: visibleProducts)
+          ],
+        );
+      }
+    });
   }
 }
